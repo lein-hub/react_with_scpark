fc295d0d42a6944dc7595fdcf4b2aae9720c3a8c
lein-hub <75424893+lein-hub@users.noreply.github.com>
2022-07-08 15:00:32 +0900

    task map

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import Header from './components/Header';
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello From React</h1>
       <Header title={"Tasks Tracker"}></Header>
+      <Tasks></Tasks>
     </div>
   );
 }
