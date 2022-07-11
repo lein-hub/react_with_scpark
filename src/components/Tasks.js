f9303cdbb004f5969b16666b458b52dd78627816
lein-hub <75424893+lein-hub@users.noreply.github.com>
2022-07-11 11:32:49 +0900

    make tasks with useState

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,27 +1,29 @@
 import React from "react";
 import Task from "./Task";
+import { useState } from "react";
 
-const tasks = [
-  {
-    id: 1,
-    text: "Doctors Appointment",
-    day: "Feb 5th at 2:30pm",
-    reminder: true,
-  },
-  {
-    id: 2,
-    text: "Meeting at school",
-    day: "Feb 6th at 1:30pm",
-    reminder: true,
-  },
-  {
-    id: 3,
-    text: "Take a test",
-    day: "Feb 7th at 2:30pm",
-    reminder: false,
-  },
-];
 const Tasks = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Meeting at school",
+      day: "Feb 6th at 1:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Take a test",
+      day: "Feb 7th at 2:30pm",
+      reminder: false,
+    },
+  ]);
+
   return (
     <div>
       <ul>
